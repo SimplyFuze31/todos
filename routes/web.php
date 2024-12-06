ee60b0d0280c5625adda5659c24eaff86a9fe45b
Kondratiuk Oleksandr <kondratuiksasha5@gmail.com>
2024-12-06 02:50:28 +0200

    Added new routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Nette\NotImplementedException;
 
 
-Route::get('/', function () {
-    return Inertia::render('Home');
-});
+Route::get('/', [TaskController::class, 'index']);
+
+Route::post('/', [TaskController::class, 'store'])->name('task.store');
