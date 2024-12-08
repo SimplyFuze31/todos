7a8c12e46db9333168f838e2e9cb1ced30c60d60
Kondratiuk Oleksandr <kondratuiksasha5@gmail.com>
2024-12-08 23:15:49 +0200

    Added task filtering

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -16,21 +16,19 @@ const tasks = ref([...props.tasks]);
 </script>
 
 <template>
-
     <section class="container mx-auto text-white ">
         <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
             <!--TO-DO-->
-            <TaskList :tasks="tasks" title="To Do" />
+            <TaskList :tasks="tasks.filter(task => task.type === 'To Do')"
+            title="To Do" />
 
             <!--In progress-->
-            <TaskList :tasks="tasks" title="In Progress" />
+            <TaskList :tasks="tasks.filter(task => task.type === 'In Progress')" title="In Progress" />
 
             <!--Completed-->
-            <TaskList :tasks="tasks" title="Completed" />
+            <TaskList :tasks="tasks.filter(task => task.type === 'Completed')" title="Completed" />
         </div>
     </section>
-
-
 </template>
 
 
